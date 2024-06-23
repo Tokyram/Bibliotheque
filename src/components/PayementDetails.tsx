6ba55b1c124d4a6628e692b4eb18b702426aecc0
mendrikafitahiana <ramilisonfita24@gmail.com>
2024-06-24 01:23:24 +0300

    Export PDF facture

--- src/components/PayementDetails.tsx
+++ src/components/PayementDetails.tsx
@@ -4,6 +4,9 @@ import { confirmCommande, getRegions } from '../api';
 import { jwtDecode } from 'jwt-decode';
 import { useNavigate } from 'react-router-dom';
 import '../styles/Modal.css';
+import '../styles/PdfCss.css';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 const PayementDetails: React.FC<any> = ({idLivre, quantite, idCommande}) => {
 
@@ -53,7 +56,7 @@ const PayementDetails: React.FC<any> = ({idLivre, quantite, idCommande}) => {
             console.log(params);
             const response = await confirmCommande(params);
             if(response.status === 200) {
-                alert(response.data.message);
+                console.log("dataConf", response.data);
                 setResponseFacture(response.data);
                 setShowModalConf(true);
                 // navigate("/")
@@ -74,6 +77,22 @@ const PayementDetails: React.FC<any> = ({idLivre, quantite, idCommande}) => {
         getRegion();
         decodedToken();
     },[]);
+
+    const generatePDF = async () => {
+        const doc = new jsPDF('p', 'mm', 'a4');
+        const content = document.getElementById('pdf-content');
+        
+        if (content) {
+            const canvas = await html2canvas(content);
+            const imgData = canvas.toDataURL('image/png');
+            const imgProps = doc.getImageProperties(imgData);
+            const pdfWidth = doc.internal.pageSize.getWidth();
+            const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+            doc.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+        }
+
+        doc.save('export.pdf');
+    };
 
   return (
     <>
@@ -137,10 +156,10 @@ const PayementDetails: React.FC<any> = ({idLivre, quantite, idCommande}) => {
             showModalConf && (
                 <div className="modal show">
                     <div className="modal-content show">
-                        <h4>Notifcations</h4>
+                        <h3>Notifcations</h3>
                         <p>Votre commande est validé et payé.</p>
-                        <p>La date de livraison sera le {responseFacture?.paiement?.dateLivraison}</p>
-                        <button className="cancel-btn">Exporter la facture en PDF</button>
+                        <p>La date de livraison sera le <b>{responseFacture?.paiement?.dateLivraison}</b></p>
+                        <button className="cancel-btn" onClick={generatePDF}>Exporter la facture en PDF</button>
                         <div className="button">
                             <button className="confirm-btn" onClick={handleContinue}>OK</button>
                         </div>
@@ -148,6 +167,45 @@ const PayementDetails: React.FC<any> = ({idLivre, quantite, idCommande}) => {
                 </div>
             )
         }
+
+        {
+            responseFacture && (
+                <div id="pdf-content" className="pdf-content">
+                    <h2>Facture</h2>
+                    <div className="header">
+                        <div className="user-info">
+                            <p><b>Nom: </b>{user?.['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'] || 'Non spécifié'}</p>
+                            <p><b>Prénom: </b>{user?.['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname'] || 'Non spécifié'}</p>
+                        </div>
+                        <div className="delivery-date">
+                            <p><b>Date de livraison: </b> {responseFacture?.paiement?.dateLivraison}</p>
+                            <p><b>Adresse: </b> {user?.['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/locality'] || 'Non spécifié'}</p>
+
+                        </div>
+                    </div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Livre</th>
+                                <th>Quantite</th>
+                                <th>Prix unitaire</th>
+                                <th>Prix total</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr key={responseFacture?.commande?.commande?.id}>
+                                <td><b>Titre: </b>{responseFacture?.commande?.commande?.Livre?.titre} <b>Auteur: </b>{responseFacture?.commande?.commande?.Livre?.auteur}</td>
+                                <td>{responseFacture?.commande?.commande?.quantite}</td>
+                                <td>{responseFacture?.commande?.commande?.Livre?.prix}</td>
+                                <td>{responseFacture?.commande?.commande?.total}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            )
+        }
+        
+
         </>
     );
 };
